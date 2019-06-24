608136d8b63c9fa01bec8149a0e4aae5b0d1abb5
Alexander <1alexvash@gmail.com>
2019-06-24 17:47:50 +0300

    fixing sounds error again

--- js/Sounds.js
+++ js/Sounds.js
@@ -1,7 +1,7 @@
-const heartSound = new Audio("../sounds/heart.mp3");
-const congratulationsSound = new Audio("../sounds/congratulations.mp3");
-const boomSound = new Audio("../sounds/boom.mp3");
-const coinSound = new Audio("../sounds/coin.mp3");
+const heartSound = new Audio("./sounds/heart.mp3");
+const congratulationsSound = new Audio("./sounds/congratulations.mp3");
+const boomSound = new Audio("./sounds/boom.mp3");
+const coinSound = new Audio("./sounds/coin.mp3");
 
 function playSound(sound) {
   if (SETTINGS.SOUNDS) {
