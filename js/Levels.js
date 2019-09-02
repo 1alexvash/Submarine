bfdbabf3b606da3540e948e2dacdc19aa4fc280e
Alexander <1alexvash@gmail.com>
2019-09-02 09:54:09 +0300

    added pictures for new levels

--- js/Levels.js
+++ js/Levels.js
@@ -1,13 +1,18 @@
 // const select = $(".Select-Level");
 
+game.totalLevels = 8;
 game.levelsOpened = 2;
 
 function initLevels() {
-  for (let i = 0; i < game.levelsOpened; i++) {
-    $$(".levels .level")[i].classList += " available";
-    $$(".levels .level .play")[i].addEventListener("click", () =>
-      initiateGame(i)
-    );
+  for (let i = 0; i < game.totalLevels; i++) {
+    $$(".levels .level")[i].style.backgroundImage = `url(images/level${i +
+      1}.jpg)`;
+    if (i < game.levelsOpened) {
+      $$(".levels .level")[i].classList += " available";
+      $$(".levels .level .play")[i].addEventListener("click", () =>
+        initiateGame(i)
+      );
+    }
   }
 }
 
